5e1eb7b6ddec37736589a23e81b6115b043828b6
Nathanphil-Z9 Bacay <nathanphilbacay@gmail.com>
2025-12-17 01:34:48 +0800

    25

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./storage/framework/views/*.php",
   ],
   theme: {
     extend: {},
