f0b28f447333cfcd214928b4552921ba5deb0a44
Nathanphil-Z9 Bacay <nathanphilbacay@gmail.com>
2025-12-17 01:33:12 +0800

    25

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js'
-      ],
-      refresh: true,
-    }),
-  ],
-  css: {
-    postcss: false // Disable PostCSS temporarily to test
-  }
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
+            refresh: true,
+        }),
+    ],
 });
