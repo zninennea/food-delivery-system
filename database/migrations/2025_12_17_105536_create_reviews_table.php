b0a0ad55064700a403ce0b23882e46cd703ee76a
Samantha Nicole Balandra <yujin.samisakim@gmail.com>
2025-12-17 15:01:39 +0800

    Update reviews migration and railway config

---
+++ database/migrations/2025_12_17_105536_create_reviews_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('user_id');
+        $table->unsignedBigInteger('restaurant_id');
+        $table->unsignedBigInteger('menu_item_id')->nullable();
+        $table->unsignedBigInteger('order_id')->nullable();
+        $table->integer('rating');
+        $table->text('comment')->nullable();
+        $table->timestamps();
+
+        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('cascade');
+        $table->foreign('menu_item_id')->references('id')->on('menu_items')->onDelete('cascade');
+        $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+    });
+    
+}
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
